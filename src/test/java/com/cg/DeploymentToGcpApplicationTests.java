71ae705ac68f004323823ec5f6a2c6905f203bd4
raghuvaran725 <kraghuvaran7097@gmail.com>
2023-11-03 20:11:32 +0530

    files added

--- src/test/java/com/cg/DeploymentToGcpApplicationTests.java
+++ src/test/java/com/cg/DeploymentToGcpApplicationTests.java
@@ -3,10 +3,10 @@ package com.cg;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class DeploymentToGcpApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
